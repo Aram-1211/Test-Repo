302685bb92b5edea09cd625707dffa2dbcead19f
Aram <kvlz9483@leeds.ac.uk>
2024-10-21 10:32:31 +0000

    Week 3 Worksheet

---
+++ week3/retaman.py
@@ -0,0 +1,9 @@
+length = int(input("Input length of Recaman Sequence: "))
+sequence = [0]
+for i in range(1, length):
+    
+        if sequence[i - 1] - i > 0 and sequence[i - 1] - 1 not in sequence:
+            sequence.append(sequence[i - 1] - i)
+        else:
+            sequence.append(sequence[i - 1] + i)
+print(sequence)
