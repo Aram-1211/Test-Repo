3384cf7f8c1ec69daad47e705fa9bd941b932bb3
Aram <kvlz9483@leeds.ac.uk>
2024-10-21 10:57:03 +0000

    Week 3 submission 3 (this time i got it)

--- recaman.py
+++ recaman.py
@@ -1,9 +1,8 @@
 length = int(input("Input length of Recaman Sequence: "))
 sequence = [0]
 for i in range(1, length):
-    
-        if sequence[i - 1] - i > 0 and sequence[i - 1] - 1 not in sequence:
+        if sequence[i - 1] - i > 0 and (sequence[i - 1] - i) not in sequence:
             sequence.append(sequence[i - 1] - i)
         else:
             sequence.append(sequence[i - 1] + i)
-print(sequence)
+print(sequence) 
