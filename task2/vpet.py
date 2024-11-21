7fc7d762d722f803e3705deccda15a88f2e03fc7
Aram <kvlz9483@leeds.ac.uk>
2024-11-21 16:52:57 +0000

    more changes

--- task2/vpet.py
+++ task2/vpet.py
@@ -6,7 +6,7 @@
 # for the __init__ method
 
 class VirtualPet:
-    def __init__(self, name, energy, hunger):
+    def __init__(self, name, energy=10, hunger=0):
         self.name = name
         self.energy = energy
         self.hunger = hunger
